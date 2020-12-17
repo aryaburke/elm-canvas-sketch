c6e441b5c179d08ccce454da0e9c006f80d72ca5
arya burke <aryaburke4@gmail.com>
2020-12-17 16:35:10 -0500

    basic grid exercises working

--- src/Grid.elm
+++ src/Grid.elm
@@ -1,11 +1,33 @@
 module Grid exposing (..)
+import Array as A
 import List
 
 type alias Cell = Bool
-type alias Grid = List (List Cell)
+type alias Grid = A.Array (A.Array Cell)
+
+--creation functions
 
 createGrid : Int -> Grid
-createGrid sides = List.map (\x -> createRow sides) (List.range 1 sides)
+createGrid sides = A.repeat sides (createRow sides)
 
-createRow : Int -> List Bool
-createRow sides = List.map (\x -> False) (List.range 1 sides)
+createRow : Int -> A.Array Cell
+createRow sides = A.repeat sides False
+
+--read functions
+getCell : (Int, Int) -> Grid -> Maybe Cell
+getCell (x,y) g = 
+    case A.get y g of
+        Just row ->
+            case A.get x row of
+                Just cell -> Just cell
+                Nothing -> Nothing
+        Nothing -> Nothing
+
+--returns the grid unchanged if (x,y) can't be found
+switchCell : (Int, Int) -> Grid -> Grid
+switchCell (x,y) g =
+    case A.get y g of
+        Just row -> case A.get x row of
+            Just cell -> A.set y (A.set x (not cell) row) g
+            Nothing -> g
+        Nothing -> g
