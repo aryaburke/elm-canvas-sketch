b05ccf2d30bd37f54abf925fa63ffec63a976031
arya burke <aryaburke4@gmail.com>
2020-12-17 21:24:16 -0500

    finished

--- src/Grid.elm
+++ src/Grid.elm
@@ -1,33 +1,34 @@
 module Grid exposing (..)
 import Array as A
 import List
+import Canvas exposing (Renderable)
 
-type alias Cell = Bool
+type alias Cell = (Bool, Renderable)
 type alias Grid = A.Array (A.Array Cell)
 
 --creation functions
 
-createGrid : Int -> Grid
-createGrid sides = A.repeat sides (createRow sides)
+createGrid : Int -> Renderable -> Grid
+createGrid sides start = A.repeat sides (createRow sides start)
 
-createRow : Int -> A.Array Cell
-createRow sides = A.repeat sides False
+createRow : Int -> Renderable -> A.Array Cell
+createRow sides start = A.repeat sides (False, start)
 
---read functions
-getCell : (Int, Int) -> Grid -> Maybe Cell
-getCell (x,y) g = 
+--returns Maybe True if cell is on, Maybe False if functions
+cellStatus : (Int, Int) -> Grid -> Maybe Bool
+cellStatus (x,y) g = 
     case A.get y g of
         Just row ->
             case A.get x row of
-                Just cell -> Just cell
+                Just (bool, render) -> Just bool
                 Nothing -> Nothing
         Nothing -> Nothing
 
 --returns the grid unchanged if (x,y) can't be found
-switchCell : (Int, Int) -> Grid -> Grid
-switchCell (x,y) g =
+setCell : (Int, Int) -> Cell -> Grid -> Grid
+setCell (x,y) val g =
     case A.get y g of
         Just row -> case A.get x row of
-            Just cell -> A.set y (A.set x (not cell) row) g
+            Just cell -> A.set y (A.set x val row) g
             Nothing -> g
         Nothing -> g
--- src/Main.elm
+++ src/Main.elm
@@ -9,14 +9,16 @@ import Html.Attributes exposing (style)
 import Html exposing (div)
 import Html.Attributes exposing (form)
 import List
+import Array as A
 import Grid
+import Html exposing (a)
 
 divSides = 500
 gridDimensions = 9 --x by x squares
 sideLength = gridDimensions / divSides
-grid = Grid.createGrid gridDimensions
-
 offColor = Color.red
+onColor = Color.green
+grid = Grid.createGrid gridDimensions
 
 view : Html msg
 view = 
@@ -28,9 +30,7 @@ view =
     [ Canvas.toHtml 
         (divSides, divSides)
         []
-        [ clearBackground
-        , renderGrid
-        ]
+        (List.append (flatten renderGrid) [ clearBackground ])
     ]
 
 clearBackground = 
@@ -40,7 +40,7 @@ renderGrid =
     List.map renderRow (floatList (List.range 0 gridDimensions))
 
 renderRow rowNum =
-    List.foldl (\i -> renderSquare offColor (i * sideLength, rowNum * sideLength)) (floatList (List.range 0 gridDimensions))
+    List.foldl (\i -> renderSquare offColor (i * sideLength, rowNum * sideLength)) [] (floatList (List.range 0 gridDimensions))
 
 renderSquare color ((x,y) as topLeftCorner) =
     shapes [ fill color ] [ rect topLeftCorner sideLength sideLength ]
@@ -48,4 +48,37 @@ renderSquare color ((x,y) as topLeftCorner) =
 floatList : List (Int) -> List (Float)
 floatList xs = List.map toFloat xs
 
+flatten : List (List a) -> List a
+flatten xs = List.foldl List.append [] xs
+
 main = view
+
+type alias Cell = (Bool, Renderable)
+type alias Grid = A.Array (A.Array Cell)
+
+--creation functions
+
+createGrid : Int -> Renderable -> Grid
+createGrid sides start = A.repeat sides (createRow sides start)
+
+createRow : Int -> Renderable -> A.Array Cell
+createRow sides start = A.repeat sides (False, start)
+
+--returns Maybe True if cell is on, Maybe False if functions
+cellStatus : (Int, Int) -> Grid -> Maybe Bool
+cellStatus (x,y) g = 
+    case A.get y g of
+        Just row ->
+            case A.get x row of
+                Just (bool, render) -> Just bool
+                Nothing -> Nothing
+        Nothing -> Nothing
+
+--returns the grid unchanged if (x,y) can't be found
+setCell : (Int, Int) -> Cell -> Grid -> Grid
+setCell (x,y) val g =
+    case A.get y g of
+        Just row -> case A.get x row of
+            Just cell -> A.set y (A.set x val row) g
+            Nothing -> g
+        Nothing -> g
